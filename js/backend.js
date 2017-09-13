fbfcf9a215d4d29a8b4debac7549a26ce8531b74
Sergey Ostapovec <seraphimaion@gmail.com>
2017-09-14 00:01:57 +0300

    переименовал переменные

--- js/backend.js
+++ js/backend.js
@@ -3,49 +3,49 @@
 (function () {
   var SERVER_URL = 'https://1510.dump.academy/keksobooking';
 
-  var createXhrRequest = function (success, error) {
+  var createXhrRequest = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.timeout = 10000;
 
-    xhr.addEventListener('load', createXhrLoadHandler(xhr, success, error));
-    xhr.addEventListener('error', createXhrErrorHandler(error));
-    xhr.addEventListener('timeout', createXhrTimeoutHandler(xhr, error));
+    xhr.addEventListener('load', createXhrLoadHandler(xhr, onSuccess, onError));
+    xhr.addEventListener('error', createXhrErrorHandler(onError));
+    xhr.addEventListener('timeout', createXhrTimeoutHandler(xhr, onError));
 
     return xhr;
   };
 
-  var createXhrLoadHandler = function (xhr, success, error) {
+  var createXhrLoadHandler = function (xhr, onSuccess, onError) {
     return function () {
       if (xhr.status === 200) {
-        success(xhr.response);
+        onSuccess(xhr.response);
       } else {
-        error('Ошибка! Код ответа: ' + xhr.status + ' ' + xhr.statusText);
+        onError('Ошибка! Код ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     };
   };
 
-  var createXhrErrorHandler = function (error) {
+  var createXhrErrorHandler = function (onError) {
     return function () {
-      error('Произошла ошибка соединения');
+      onError('Произошла ошибка соединения');
     };
   };
 
-  var createXhrTimeoutHandler = function (xhr, error) {
+  var createXhrTimeoutHandler = function (xhr, onError) {
     return function () {
-      error('Запрос не успел выполниться за: ' + xhr.timeout + 'ms');
+      onError('Запрос не успел выполниться за: ' + xhr.timeout + 'ms');
     };
   };
 
   window.backend = {
-    load: function (success, error) {
-      var xhr = createXhrRequest(success, error);
+    load: function (onSuccess, onError) {
+      var xhr = createXhrRequest(onSuccess, onError);
 
       xhr.open('GET', SERVER_URL + '/data');
       xhr.send();
     },
-    save: function (data, success, error) {
-      var xhr = createXhrRequest(success, error);
+    save: function (data, onSuccess, onError) {
+      var xhr = createXhrRequest(onSuccess, onError);
 
       xhr.open('POST', SERVER_URL);
       xhr.send(data);
